99b9a7e4f30ed875b01b25df6339ddf6481720ba
gfike19 <gfike19@gmail.com>
2024-05-02 15:32:03 -0500

    5/2/2024 - create branch for staircase problem. Started work on problem. Current solution not working

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        int totalCharCount = 4;
+        int starCount = 1;
+        int spaceCount = 3;
 
+        while(starCount != 4) {
+            for(int i = 0; i <= spaceCount;i ++) {
+                System.out.print(' ');
+            }
+
+            for(int i = 0; i <= starCount; i++) {
+                System.out.print('*');
+            }
+            System.out.print('\n');
+            starCount--;
+        }
     }
-    
 }
